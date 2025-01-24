8906ec64069b76140eefdfce460ab5bdfba2c672
Bryan Khor <bryankhor07@gmail.com>
2025-01-23 18:51:36 -0800

    Implemented dark mode on all pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
-import { InstagramIcon, GithubIcon, LinkedInIcon } from "./Icons";
+import {
+  InstagramIcon,
+  GithubIcon,
+  LinkedInIcon,
+  SunIcon,
+  MoonIcon,
+} from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -13,7 +20,7 @@ const CustomLink = ({ href, title, className = "" }) => {
       {title}
       <span
         className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 
-        group-hover:w-full transition-[width] ease duration-300 ${
+        group-hover:w-full transition-[width] ease duration-300 dark:bg-light ${
           router.asPath === href ? "w-full" : "w-0"
         }`}
       >
@@ -24,8 +31,10 @@ const CustomLink = ({ href, title, className = "" }) => {
 };
 
 const NavBar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -56,10 +65,23 @@ const NavBar = () => {
           target={"_blank"}
           whileHover={{ y: -2 }}
           whileTap={{ scale: 0.9 }}
-          className="w-6 ml-3"
+          className="w-6 mx-3"
         >
           <InstagramIcon />
         </motion.a>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={`ml-3 flex items-center
+        justify-center rounded-full p-1 ${
+          mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+        }`}
+        >
+          {mode === "dark" ? (
+            <SunIcon className={"fill-dark"} />
+          ) : (
+            <MoonIcon className={"fill-dark"} />
+          )}
+        </button>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
